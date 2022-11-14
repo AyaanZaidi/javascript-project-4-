4bff0ad6f23af7b257db69427b7d9ab5cc86a864
AyaanZaidi <103330931+AyaanZaidi@users.noreply.github.com>
2022-11-14 16:57:28 +0500

    javascript assignment 4
    for practice

---
+++ app.js
@@ -0,0 +1,180 @@
+alert("welcome to my alert")
+
+alert("Hello World! \n This is my string")
+
+alert("welcome to js land...")
+
+alert("Happy coding! \nprevent this page from creating addditional dialogs")
+
+alert("hello ... I can run js through my web browser's console")
+
+// chap 1 is completed;
+
+var userName;
+var myName = "syed ayaan hussain zaidi"
+
+var message = "we working on programming"
+
+var title = "hello world"
+
+alert(title)
+
+alert(myName)
+
+var age = "16 year old"
+alert(age)
+
+var learning = "certified mobile application development"
+alert(learning)
+
+var restaurant = "'pizza\npizz\npiz\npi\np"
+alert(restaurant)
+
+var email = "ayaanzaidi006@gmail.com"
+alert("This is my gmail address"+ " " +email)
+
+var book = "A smarter way to learn javascript"
+alert("I am trying to learn from the book" + " " + book)
+
+document.write("YAH! I can write HTML content through javascript")
+
+var paste = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(paste)
+// chp 2 is completed
+
+
+var dob = "16 year old"
+alert(dob)
+
+var check = "You have visited this site 12 times"
+alert(check)
+
+var birth_Year = "2006"
+
+document.write(" my birthYear is" + " " + birth_Year)
+
+
+var visitorsName = "Ayaan hussain"
+var productTitle = "T,shirts"
+var quantity = "5 T,shirts"
+document.write( " " + visitorsName + " ordered " + quantity +"(s) on xyz clothing store" )
+// chp 3 is completed
+
+var legal = "ayaan";
+var illegal = "ayaan";
+var browser = "ayaan";
+
+var legal1 = "ayaan zaidi" // legal variable
+var legal_2 = "ayaan zaidi" // legal variable
+var legal$3 = "ayaan zaidi" // legal variable
+var $legal4 = "ayaan zaidi" // legal variable
+var legalFive = "ayaan zaidi" // legal variable
+
+// var 1legal = "ayaan zaidi"  illegal variable
+// var leg al = "ayaan zaidi"  illegal variable
+// var legal*3 = "ayaan zaidi"  illegal variable
+// var var = "ayaan zaidi"  illegal variable
+// var lega@five = "ayaan zaidi"  illegal variable
+
+document.write("<h1>“Rules for naming JS variables”</h1>")
+
+
+
+var abc = "number"
+var my = "$my_1stVariable"
+
+document.write("Variable names can only contain ," + abc + ",$ and _ . For example :" + my)
+
+var world = "letter";
+var fun = "$";
+var line = "_ .";
+document.write(" variable must begin with a " + world + "," + fun + "or" + line + " For example: $name, _name or name")
+
+
+var sensi = "sensitive"
+document.write(" Variable names are case " + sensi)
+
+var keys = "keywords"
+document.write(" Variable names should not be JS " + keys)
+// chap 4 completed
+
+var math = 5 + 3;
+document.write(math);
+
+var sci = 5 - 3;
+document.write(sci);
+
+var eng = 5 * 3;
+document.write(eng);
+
+var urdu = 5 / 3;
+document.write(urdu);
+
+
+var duff;
+
+var ayaan = "zaidi";
+document.write(ayaan);
+
+var i = 1 + 1 + 1 + 1 +1+7
+document.write(++i)
+var i;
+document.write(--i)
+
+
+var cost = 600 * 5
+document.write("Total cost to buy tickets to a movie is " + cost + "PKR")
+
+
+var number = parseInt(prompt("enter your integer"))
+
+for(var i = 1; i<=10 ;i++){
+    var resullt = i*number
+    document.write(` &nbsp  &nbsp  ${number}   x    ${i} =  ${resullt}<br>`);
+}
+
+var priceItem = 650;
+var quantityItem = 3;
+var pritem = 100;
+var quantityItems = 7;
+var charges = 100;
+
+
+document.write("priceItem = 650<br>" + "quantityItem = 3<br>" + "pritem = 100<br>" + "quantityItems = 7<br>" + "charges = 100<br>")
+
+
+var e = priceItem * quantityItem
+var j = pritem * quantityItems
+var t = e+j+charges
+document.write("Total cost of your order is " + t)
+console.log(t)
+console.log(e)
+console.log(j)
+
+document.write("<h1>Marks Sheet</h1>")
+
+var totalMarks = 980;
+var obtain = 804;
+var percent = totalMarks / obtain * 100
+document.write("totalMarks = 980<br>" + "obtain = 804<br>" + percent)
+
+
+var random = 6;
+var add = random + 5 * 10 / 2;
+console.log(add)
+
+var currentYear = 2022
+var birthYear = 2006
+var year = currentYear - birthYear
+console.log(year)
+
+
+
+
+
+
+
+
+
+
+
